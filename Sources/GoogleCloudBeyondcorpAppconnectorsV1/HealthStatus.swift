cffe75e0768c14837bc5a724ac1ce37ce9917254
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudBeyondcorpAppconnectorsV1/HealthStatus.swift
+++ Sources/GoogleCloudBeyondcorpAppconnectorsV1/HealthStatus.swift
@@ -77,7 +77,7 @@ public enum HealthStatus: Codable, Equatable, Sendable {
 
   /// Initialize from a string value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownStringValue`](doc:HealthStatus/unknownStringValue(_:)).
   public init(stringValue: Swift.String) {
     switch stringValue {
     case "HEALTH_STATUS_UNSPECIFIED": self = .unspecified
@@ -91,7 +91,7 @@ public enum HealthStatus: Codable, Equatable, Sendable {
 
   /// Initialize from an integer value.
   ///
-  /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+  /// If the value is unknown, this initializes to [`unknownIntValue`](doc:HealthStatus/unknownIntValue(_:)).
   public init(intValue: Int) {
     switch intValue {
     case 0: self = .unspecified
